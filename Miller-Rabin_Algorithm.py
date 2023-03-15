8aa0134f8c8c758b784951e2b831d24beac1d2b5
Edward Calzia <84807135+EdwardCalzia@users.noreply.github.com>
2023-03-15 22:27:31 +0000

    Create Miller-Rabin_Algorithm.py

---
+++ Miller-Rabin_Algorithm.py
@@ -0,0 +1,23 @@
+import random
+
+def is_prime(n, k=5):
+    if n <= 3:
+        return n == 2 or n == 3
+
+    r, d = 0, n - 1
+    while d % 2 == 0:
+        r += 1
+        d //= 2
+
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, d, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
